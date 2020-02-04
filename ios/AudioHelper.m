50a8d62a26e87d35d44055714e7975ab5ddf3f7e
imronimamwg <imronimam11@gmail.com>
2020-02-04 12:32:48 +0700

    printer star ios to react native audio helper

--- ios/AudioHelper.m
+++ ios/AudioHelper.m
@@ -1,7 +1,7 @@
 #import <React/RCTBridgeModule.h>
 
 // bridging with swift
-@interface RCT_EXTERN_MODULE(PrinterStarIos, NSObject)\
+@interface RCT_EXTERN_MODULE(AudioHelper, NSObject)\
     RCT_EXTERN_METHOD(callbackMethod:(RCTResponseSenderBlock)callback)
     RCT_EXTERN_METHOD(simpleMethod:(NSString *)message)
 @end
